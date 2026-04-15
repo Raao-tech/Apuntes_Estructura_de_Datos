e537cad7b7eadb3cf6d05a5645bcec84415c9ce0
Raao <rafaelaraujordono@gmail.com>
2026-04-15 14:19:09 +0000

    Creado el create_TAD.sh

---
+++ Trees/trees.h
@@ -0,0 +1,6 @@
+#ifndef TREES_H
+#define TREES_H
+
+//Tu código aquí
+
+#endif
---
+++ creat_TAD.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Descripción: Este script permite crear un los archivos .c/.h necesarios
+#              para la creación de un TAD de forma automática.
+# Autor: Rafael Araujo
+# Fecha: 15-04-2025
+
+read -p "¿Cómo se llamará el nuevo TAD? >" name_tad;
+echo -e "¿En que directorio?\033[1;35m";
+
+ls -d */ 2>/dev/null
+
+echo -e "\033[0m";
+
+read -p "¿Directorio? >>>" dir;
+
+[ ! -d ${dir} ] && mkdir  ./${dir}/
+if [[ -f "./${dir}/${name_tad}.h"   ||  -f "./${dir}/${name_tad}.c" ]]; then
+    echo -e "Disculpa, pero ya existe un tad con esa ubicación y nombre\n"
+    exit 1;
+fi
+
+touch  ./${dir}/${name_tad}.h
+touch  ./${dir}/${name_tad}.c
+
+
+echo -e "#ifndef ${name_tad^^}_H\n#define ${name_tad^^}_H\n\n//Tu código aquí\n\n#endif" >| "./${dir}/${name_tad}.h"
+echo -e "#include <stdio.h>\n#include \"${name_tad}.h\"\n" >| "./${dir}/${name_tad}.c"
+
+echo -e "Todo listo ;D\n"
